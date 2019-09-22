047d15f1ba5ecc971887d69600c15dbe02001d71
tanaka <nakatanakatana@gmail.com>
2019-09-22 21:46:07 +0900

    update vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
+const path = require('path');
 module.exports = {
-  publicPath: 'excel-document-reader/'
+  productionSourceMap: false,
+  configureWebpack: {
+    devtool: process.env.NODE_ENV === 'production' ? false : 'source-map',
+    resolve: {
+      alias: {
+        '@app': path.join(__dirname, './src')
+      }
+    }
+  },
+  publicPath:
+    process.env.NODE_ENV === 'production' ? 'excel-document-reader/' : '/'
 };
